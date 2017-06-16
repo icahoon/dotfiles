c0d33344a058deff4a8b2ed1ac46a0f6b1fc26cb
Ian Cahoon <icahoon@cisco.com>
2017-06-16 11:46:15 -0600

    add pull and push alias

--- .rc.d/git.sh
+++ .rc.d/git.sh
@@ -10,5 +10,6 @@ alias uncommit="git reset --soft HEAD~1"
 
 alias pull="git pull --rebase"
 alias push="git push origin HEAD:refs/for/master"
+alias pp="pull && push"
 
 alias hooks='if [ ! -f .git/hooks/commit-msg ]; then scp -q -p -P $gerrit_port $gerrit:hooks/commit-msg .git/hooks/; fi'
