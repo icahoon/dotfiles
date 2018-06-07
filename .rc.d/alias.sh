a796aa5d65dfaaaa931c3e31ffeda693f78b7e82
Ian Cahoon <ian.cahoon@jumpcloud.com>
2018-06-07 12:47:04 -0600

    add go aliases

--- .rc.d/alias.sh
+++ .rc.d/alias.sh
@@ -9,6 +9,12 @@ alias cd..='cd ..'
 alias -- -='cd - &>/dev/null'
 alias ~='cd ~'
 alias today='date +%Y-%m-%d'
+alias lsgo='ls -1 *.go | grep -v _test'
+alias lstest='ls -1 *_test.go'
+alias llgo='ls -l *.go | grep -v _test'
+alias lltest='ls -l *_test.go'
+alias vigo='vi $(lsgo)'
+alias vitest='vi $(lstest)'
 
 # Typos
 #----------------------------------
